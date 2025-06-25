8009f1d79f23c5c6f65c944a4b7b9dc72b16bb0b
Pedram Ghane <p.ghane@gmail.com>
2025-06-25 10:56:43 +0200

    refactor collection page

--- src/app/(site)/collection/page.tsx
+++ src/app/(site)/collection/page.tsx
@@ -1,141 +1,97 @@
 // src/app/(site)/collection/page.tsx
 "use client";
 
-import { cn } from "@/lib/utils";
+import { handleClientError } from "@/lib/handleClientError";
+import { Category } from "@/types";
 import type { Content } from "@/types/content";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 // UI Imports
-import { HomeContentCard } from "@/app/(site)/components/ContentCard";
+import { ContentCard } from "@/app/(site)/components/ContentCard";
+import { ContentCategoryFilter } from "@/app/(site)/components/ContentCategoryFilter";
 import Loader from "@/components/Loader";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/theme/carousel";
-import { RowsIcon } from "@phosphor-icons/react/dist/ssr";
-import { handleClientError } from "@/lib/handleClientError";
 
 export default function CollectionPage() {
   const [loading, setLoading] = useState(false);
   const [contents, setContents] = useState<Content[]>([]);
-  const [activeType, setActiveType] = useState<string | null>(null);
-  const [changeBookmark, setChangeBookmark] = useState(0);
-  // const usedTypes = new Set<string>(contents.map((c) => c.type));
-  // const filterItems = [
-  //   { value: null, label: "All", icon: RowsIcon },
-  //   ...contentTypes.filter((type) => usedTypes.has(type.value)),
-  // ];
+  const [activeCategory, setActiveCategory] = useState<string | null>("");
 
-  function handleFilterClick(type: string | null) {
-    setActiveType(type);
-  }
+  const fetchContents = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get("/api/contents");
+      const bookmarkedContent = res.data.filter(
+        (c: Content) => c.actions?.bookmark,
+      );
 
-  // const getContentTypeMeta = (value: string) => {
-  //   return contentTypes.find((c) => c.value === value);
-  // };
-
+      setContents(bookmarkedContent);
+    } catch (err) {
+      handleClientError(err, "Failed to fetch contents.");
+    } finally {
+      setLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchContents = async () => {
-      setLoading(true);
-
-      try {
-        const res = await axios.get(`/api/bookmarks`, {
-          withCredentials: true,
-        });
-        setContents(res.data);
-      } catch (error) {
-        handleClientError(error, "Failed to fetch bookmarks.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchContents();
-  }, [changeBookmark]);
+  }, []);
+
+  const usedCategories = useMemo(
+    () =>
+      Array.from(
+        new Map(contents.map((c) => [c.category?.id, c.category])).values(),
+      ).filter(Boolean),
+    [contents],
+  );
+
+  const filteredContents = useMemo(() => {
+    if (!activeCategory) return contents;
+    return contents.filter((c) => c.category?.id === activeCategory);
+  }, [contents, activeCategory]);
+
+  const categoriesWithAll = [
+    { id: "", label: "All", icon: "FolderIcon" },
+    ...usedCategories,
+  ];
+
+  if (loading) return <Loader />;
 
   return (
-    <main className="flex flex-1 flex-col">
-      {/* {filterItems.length > 1 && (
-        <div className="_carousel mx-auto max-w-full md:max-w-4xl md:px-2.5">
-          <Carousel
-            opts={{
-              align: "center",
-            }}
-            className="w-full py-4 duration-300 md:py-5"
-          >
-            <CarouselContent>
-              {filterItems.map((type) => {
-                const Icon = type.icon;
+    <div className="_explore-page flex flex-1 flex-col">
+      <div className="container mx-auto h-full max-w-5xl px-4">
+        <ContentCategoryFilter
+          filterItems={categoriesWithAll as Category[]}
+          activeCategory={activeCategory}
+          setActiveCategory={setActiveCategory}
+        />
 
-                return (
-                  <CarouselItem
-                    key={type.value}
-                    className="basis-2/5 md:basis-auto"
-                    onClick={() => handleFilterClick(type.value)}
-                  >
-                    <div
-                      className={cn(
-                        "flex cursor-pointer flex-col items-center justify-center gap-1 rounded-md border border-gray-300 bg-gray-50/50 p-2 duration-300 hover:text-rose-600 md:h-12 md:flex-row md:border-white md:bg-white md:px-4 md:hover:border-gray-200/70 md:hover:bg-gray-50",
-                        activeType === type.value &&
-                          "border font-semibold text-rose-600 md:border-gray-200/70 md:bg-gray-50",
-                      )}
-                    >
-                      <Icon size={20} /> {type.label}
-                    </div>
-                  </CarouselItem>
-                );
-              })}
-            </CarouselContent>
-            <CarouselPrevious className="hidden md:flex" />
-            <CarouselNext className="hidden md:flex" />
-          </Carousel>
-        </div>
-      )}
-
-      <div className="_content flex flex-1 flex-col">
-        <div className="container mx-auto flex max-w-4xl flex-1 flex-col px-4">
-          {loading ? (
-            <Loader />
-          ) : (
-            <div className="grid flex-1 content-start gap-4 md:grid-cols-2">
-              {!loading && contents.length === 0 && (
-                <div className="col-span-2 flex flex-1 items-center justify-center text-gray-500">
-                  There are no collected items.
-                </div>
-              )}
-
-              {contents
+        <div className="_content flex flex-1 flex-col py-4">
+          <div className="grid flex-1 grid-cols-2 content-start gap-3 md:grid-cols-4 md:gap-5">
+            {filteredContents.length === 0 ? (
+              <div className="col-span-2 flex flex-1 items-center justify-center text-gray-500">
+                There are no contents to show.
+              </div>
+            ) : (
+              filteredContents
                 .slice()
                 .sort(
                   (a, b) =>
                     new Date(b.createdAt).getTime() -
                     new Date(a.createdAt).getTime(),
                 )
-                .filter((content) => !activeType || content.type === activeType)
                 .map((content) => {
-                  const meta = getContentTypeMeta(content.type);
-                  const Icon = meta?.icon;
-
                   return (
-                    <HomeContentCard
+                    <ContentCard
                       key={content.id}
                       content={content}
-                      Icon={Icon}
-                      meta={meta}
-                      onChangeBookmark={() =>
-                        setChangeBookmark(changeBookmark + 1)
-                      }
+                      onChangeBookmark={fetchContents}
                     />
                   );
-                })}
-            </div>
-          )}
+                })
+            )}
+          </div>
         </div>
-      </div> */}
-    </main>
+      </div>
+    </div>
   );
 }
