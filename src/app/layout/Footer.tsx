84d4e555b0f4335adc16b4a9aecb60308273fdb2
Pedram Ghane <p.ghane@gmail.com>
2025-06-25 20:16:28 +0200

    (static content): footer text updated

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -4,8 +4,16 @@ export const Footer = () => {
     <footer>
       <div className="continer mx-auto h-full max-w-5xl px-4">
         <div className="flex h-16 items-center justify-center">
-          <p className="text-muted-foreground text-sm">
-            &copy; 2025 Pickly. All rights reserved.
+          <p className="text-muted-foreground text-[15px]">
+            &copy; 2025 Pickly. Desing with 🔥 by{" "}
+            <a
+              className="underline underline-offset-3 hover:text-black"
+              href="https://github.com/iampedi"
+              target="_blank"
+            >
+              Pediland
+            </a>
+            .
           </p>
         </div>
       </div>
